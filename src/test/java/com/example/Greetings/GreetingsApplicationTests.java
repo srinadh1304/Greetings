33466dc87a38610047547a5d7d29f28bed0186c1
srinadh1304 <srinadh1304@gmail.com>
2021-11-01 07:40:50 +0530

    ADDED: Initial commit

---
+++ src/test/java/com/example/Greetings/GreetingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Greetings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
