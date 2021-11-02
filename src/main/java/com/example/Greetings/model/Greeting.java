556394509c9c97e22b3f3a96044b01b9112094aa
srinadh1304 <srinadh1304@gmail.com>
2021-11-02 06:13:53 +0530

    ADDED: model class for greetings

--- src/main/java/com/example/Greetings/model/Greeting.java
+++ src/main/java/com/example/Greetings/model/Greeting.java
@@ -1,8 +1,23 @@
 package com.example.Greetings.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="GREETINGS")
 public class Greeting {
+	
+	@Id
 	private long id;
 	private String content;
+	
+	public Greeting() {
+		
+		this.id = 0;
+		this.content = "";
+	}
 	public long getId() {
 		return id;
 	}
