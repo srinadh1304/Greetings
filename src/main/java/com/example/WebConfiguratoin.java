79b18b0e859eab577ade30bc894612569711c484
srinadh1304 <srinadh1304@gmail.com>
2021-11-02 06:28:09 +0530

    ADDED: configuration for web

---
+++ src/main/java/com/example/WebConfiguratoin.java
@@ -0,0 +1,19 @@
+package com.example;
+
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfiguratoin {
+
+	 @Bean
+	    ServletRegistrationBean h2servletRegistration(){
+	        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+	        registrationBean.addUrlMappings("/console/*");
+	        return registrationBean;
+	    }
+
+}
