3e8efe702085e09cbf83862c517742e7f45f0a45
srinadh1304 <srinadh1304@gmail.com>
2021-11-02 18:14:32 +0530

    A DDED: rest call to edit a greeting message

--- src/main/java/com/example/Greetings/controller/GreetingController.java
+++ src/main/java/com/example/Greetings/controller/GreetingController.java
@@ -1,20 +1,13 @@
 package com.example.Greetings.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,23 +32,28 @@ public class GreetingController {
 	 }
 
 	 
-	 @PostMapping("/postGreeting")
-	    public Greeting sayHello(@RequestBody Greeting greeting){
-	        return new Greeting(counter.incrementAndGet(),String.format(template,greeting.getContent()));
-	 }
-	 
-	 @PutMapping("/putGreeting/{counter}")
-	    public Greeting sayHello(@PathVariable int counter,@RequestParam(value = "content")String contentName){
-	        return new Greeting(counter,String.format(template,contentName));
-	 }
-	 @GetMapping(value = "/getGreetingByID")
-		public ResponseEntity<String> getEmployeeByID(@RequestParam(name = "id") int id) {
-			return new ResponseEntity(greetingService.getGreetingById(id), HttpStatus.OK);
-		}
-	 @GetMapping(value = "/getgreetings")
-		public ResponseEntity<String> getGreetings() {
-			return new ResponseEntity(greetingService.getGreetings(), HttpStatus.OK);
-	}
+	 @GetMapping(value = {"/greetingid"})
+	    public Greeting getGreetingByID(@RequestParam(value="id",defaultValue="World") long id)
+	    {
+	        return greetingService.getGreetingById(id);
+	    }
+	    
+	    @GetMapping("/greetinglist")
+	    public List<Greeting> getGreetingList()
+	    {
+	    	return greetingService.getGreetings();
+	    }
+	    
+	    @PutMapping("/update/{name1}/{name2}")
+	    public Greeting editGreeting(@PathVariable String name1, @PathVariable String name2)
+		{
+	        return greetingService.editGreeting(name1, name2);
+	    }
 
+	    @PutMapping("put/{name}")
+	    public Greeting sayHello(@PathVariable String name)
+		{
+	        return new Greeting(counter.incrementAndGet(),String.format(template, name));
+	    }
 	 
 }
