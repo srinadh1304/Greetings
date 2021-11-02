0a4e6c61f9bf90e4bee8d3e5bd9e3fceb6ee5b1f
srinadh1304 <srinadh1304@gmail.com>
2021-11-02 06:59:31 +0530

    ADDED: method declaration to get all greeting messages

--- src/main/java/com/example/Greetings/service/IGreetingService.java
+++ src/main/java/com/example/Greetings/service/IGreetingService.java
@@ -3,8 +3,10 @@ package com.example.Greetings.service;
 import com.example.Greetings.model.Greeting;
 import com.example.Greetings.model.User;
 
+import java.util.List;
 import java.util.Optional;
 public interface IGreetingService {
 	Greeting addGreeting(User user);
 	Optional<Greeting> getGreetingById(long id);
+	List<Greeting> getGreetings();
 }
